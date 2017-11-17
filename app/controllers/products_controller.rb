300c4a225f273a3fc919c0e4a77b79957cad99a7
nevergiveup2212 <blackcat22121996@gmail.com>
2017-11-17 17:15:37 +0700

    fixBug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,7 +74,7 @@ class ProductsController < ApplicationController
         end
       end
     end
-  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_cable.disable_request_forgery_protection = true
 
   Depot::Application.configure do
-    config.action_mailer.delivery_method = :test
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { address:'smtp.gmail.com',
                                            port:587,
                                            domain:'domain.of.sender.net',
